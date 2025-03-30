745386670c2567baccfa31e9558a226541152659
William Cairns <cairnswm@gmail.com>
2025-03-30 17:11:27 +0200

    Force PHP Error message display

--- php/corsheaders.php
+++ php/corsheaders.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 	header("Access-Control-Allow-Origin: *");
 	header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, OPTIONS');
