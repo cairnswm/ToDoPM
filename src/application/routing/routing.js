1e1fb4b5c36f9e83d7f9a2c82b1c9c38398f7340
William Cairns <cairnswm@gmail.com>
2025-03-30 17:07:41 +0200

    File Name Case Fix

--- src/application/routing/routing.js
+++ src/application/routing/routing.js
@@ -14,7 +14,7 @@ import ProtectedRoute from '../../auth/components/ProtectedRoute';
 import PublicRoute from '../../auth/components/PublicRoute';
 import AdminRoute from '../../auth/components/AdminRoute';
 import Admin from '../../auth/pages/Admin';
-import ComingsoonPage from '../../auth/pages/ComingsoonPage';
+import ComingsoonPage from '../../auth/pages/ComingSoonPage';
 import DashboardPage from '../pages/DashboardPage';
 const SubscriptionRouting = React.lazy(() => import('../../auth/routing/subscriptionrouting'));
 
